56d6b609ef2d6769d46c71c3d45fdb7fad894b36
hemangdigitalxplode <hemang@digitalxplode.com>
2024-10-30 22:02:06 +0530

    home banner done

---
+++ src/Header&Footer/Footer.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    
+    <h1>This is Footer</h1>
+    
+    
+    </>
+  )
+}
+
+export default Footer
