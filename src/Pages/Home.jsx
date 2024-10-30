ac15cf1c2e631001328953f3f25fff316ab21b4c
hemangdigitalxplode <hemang@digitalxplode.com>
2024-10-31 01:28:39 +0530

    popup modal added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -116,7 +117,7 @@ const Home = () => {
                             and meditation, sharp your yoga with
                             astsankhlam</p>
                         <div className='mt-[20px]'>
-                            <button className="px-2 py-2 md:px-4 md:py-2 rounded-sm bg-[#cbb799] text-white font-semibold text-[10px] md:text-[20px]">
+                            <button type="button" data-modal-target="authentication-modal-2" data-modal-toggle="authentication-modal-2" className="px-2 py-2 md:px-4 md:py-2 rounded-sm bg-[#cbb799] text-white font-semibold text-[10px] md:text-[20px]">
                                 BOOK A DEMO
                             </button>
                         </div>
@@ -126,6 +127,50 @@ const Home = () => {
                         <img style={{ filter: 'brightness(50%)' }} src={AboutImg} alt="" />
                     </div>
 
+                </div>
+                {/* <!-- Main modal --> */}
+                <div id="authentication-modal-2" tabindex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div className="relative p-4 w-full max-w-md max-h-full">
+                        {/* <!-- Modal content --> */}
+                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            {/* <!-- Modal header --> */}
+                            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                    Book Your Demo Slot Now!
+                                </h3>
+                                <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            {/* <!-- Modal body --> */}
+                            <div className="p-4 md:p-5">
+                                <form className="space-y-4" action="#">
+                                    <div>
+                                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                                    </div>
+                                    <div>
+                                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Age</label>
+                                        <input type="password" name="password" id="password" placeholder="Ex:21yr" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                    </div>
+                                    <div>
+                                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Profession</label>
+                                        <input type="password" name="password" id="password" placeholder="Ex:Private Employee" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                    </div>
+                                    <div>
+                                        <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Mobile No.</label>
+                                        <input type="password" name="password" id="password" placeholder="+91 897854##" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                                    </div>
+
+                                    <button type="submit" className="w-full text-white bg-[#cbb799]  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  ">Book Now</button>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
